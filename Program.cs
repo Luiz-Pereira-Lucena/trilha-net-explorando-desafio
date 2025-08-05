decd110d8c95d455f75a0171400e9d2937eddd75
Luiz Pereira Lucena Neto <luizpereiraln@hotmail.com>
2025-08-05 16:30:54 -0300

    Atualiza lógica de reserva e cálculo de diária
    Implementa verificação de capacidade de hóspedes na classe `Reserva`, lançando exceção quando necessário. O método `ObterQuantidadeHospedes` agora retorna a quantidade real de hóspedes. O cálculo do valor da diária foi ajustado para aplicar um desconto de 10% para reservas superiores a 10 dias.
    
    No arquivo `Program.cs`, a criação da suíte foi alterada para usar um valor decimal. A lógica de criação da reserva foi modificada para incluir o cadastro da suíte e dos hóspedes antes da exibição dos resultados, que agora estão formatados corretamente. Linhas de código desnecessárias foram comentadas.

--- Program.cs
+++ Program.cs
@@ -13,13 +13,42 @@ hospedes.Add(p1);
 hospedes.Add(p2);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30m);
 
-// Cria uma nova reserva, passando a suíte e os hóspedes
+// Cria uma nova reserva, passando os dias reservados
 Reserva reserva = new Reserva(diasReservados: 5);
+
+// Cadastra a suíte e os hóspedes
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
 // Exibe a quantidade de hóspedes e o valor da diária
 Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
-Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
+Console.WriteLine($"Valor total da reserva: R$ {reserva.CalcularValorDiaria():0.00}");
+
+
+//using System.Text;
+//using DesafioProjetoHospedagem.Models;
+
+//Console.OutputEncoding = Encoding.UTF8;
+
+//// Cria os modelos de hóspedes e cadastra na lista de hóspedes
+//List<Pessoa> hospedes = new List<Pessoa>();
+
+//Pessoa p1 = new Pessoa(nome: "Hóspede 1");
+//Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+
+//hospedes.Add(p1);
+//hospedes.Add(p2);
+
+//// Cria a suíte
+//Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+
+//// Cria uma nova reserva, passando a suíte e os hóspedes
+//Reserva reserva = new Reserva(diasReservados: 5);
+//reserva.CadastrarSuite(suite);
+//reserva.CadastrarHospedes(hospedes);
+
+//// Exibe a quantidade de hóspedes e o valor da diária
+//Console.WriteLine($"Hóspedes: {reserva.ObterQuantidadeHospedes()}");
+//Console.WriteLine($"Valor diária: {reserva.CalcularValorDiaria()}");
